19537a79a0a8b43d1312fdd18b6d0edea66cbcc8
nobodyAtall <tsogkas@ceid.upatras.gr>
2011-08-10 20:29:46 +0300

    Removing mddi module, this is device specific.

--- msm7x27.mk
+++ msm7x27.mk
@@ -74,7 +74,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/semc/msm7x27-common/modules/sdio.ko:system/lib/modules/sdio.ko \
     device/semc/msm7x27-common/modules/tiwlan_drv.ko:system/lib/modules/tiwlan_drv.ko \
-    device/semc/msm7x27-common/modules/x8mddi.ko:system/lib/modules/x8mddi.ko \
     device/semc/msm7x27-common/modules/dm-mod.ko:system/lib/modules/dm-mod.ko \
     device/semc/msm7x27-common/modules/dm-crypt.ko:system/lib/modules/dm-crypt.ko \
     device/semc/msm7x27-common/modules/twofish.ko:system/lib/modules/twofish.ko \
